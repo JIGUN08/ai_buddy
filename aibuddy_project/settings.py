13597718331750ce87301c8f7186fdaadcc90ec2
JIGUN08 <skycenter205@gmail.com>
2025-10-14 10:26:53 +0900

    Update settings.py

--- aibuddy_project/settings.py
+++ aibuddy_project/settings.py
@@ -1,12 +1,10 @@
 import os
 from pathlib import Path
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Load environment variables from .env file
-from dotenv import load_dotenv
-load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,10 +65,9 @@ WSGI_APPLICATION = "aibuddy_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
